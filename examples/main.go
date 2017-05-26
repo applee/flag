e7e462a950b064eeb46960996cd6305f9891e2a4
Applee <applee@gmail.com>
2017-05-26 13:25:00 +0800

    Add more examples

--- examples/main.go
+++ examples/main.go
@@ -3,18 +3,22 @@ package main
 import (
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/applee/flag"
 )
 
 func main() {
 	var (
-		config     string
-		length     float64
-		age        int
-		name       string
-		female     bool
-		extHobbies string
+		config              string
+		length              float64
+		age                 int
+		name                string
+		female              bool
+		extHobbies          string
+		extLinkmanParentDad string
+		extLinkmanParentMom string
+		duration            time.Duration
 	)
 
 	flag.StringVar(&config, "config", "", "help message")
@@ -22,7 +26,10 @@ func main() {
 	flag.IntVar(&age, "age", 0, "help message")
 	flag.Float64Var(&length, "length", 0, "help message")
 	flag.BoolVar(&female, "female", false, "help message")
+	flag.DurationVar(&duration, "duration", time.Second*5, "")
 	flag.StringVar(&extHobbies, "ext.hobbies", "", "")
+	flag.StringVar(&extLinkmanParentDad, "ext.linkman.parent.dad", "", "")
+	flag.StringVar(&extLinkmanParentMom, "ext.linkman.parent.mom", "", "")
 
 	err := flag.Parse()
 	if err != nil {
@@ -34,4 +41,7 @@ func main() {
 	fmt.Println("name:", name)
 	fmt.Println("female:", female)
 	fmt.Println("hobbies:", extHobbies)
+	fmt.Println("duration", duration.Seconds())
+	fmt.Println("dad", extLinkmanParentDad)
+	fmt.Println("mom", extLinkmanParentMom)
 }
