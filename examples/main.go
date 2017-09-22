39e6c4e05792de04c834e75c3d5a15285f97b7e8
Applee <applee@gmail.com>
2017-09-22 11:21:18 +0800

    feat: support array value in .toml and commandline

--- examples/main.go
+++ examples/main.go
@@ -1,12 +1,35 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"time"
 
 	"github.com/applee/flag"
 )
+
+type hobbies []string
+
+func (p *hobbies) String() string {
+	return fmt.Sprint(*p)
+}
+
+func (p *hobbies) Set(value string) error {
+	var err error
+	b := []byte(value)
+	if IsJSON(b) {
+		err = json.Unmarshal(b, p)
+	} else {
+		*p = append(*p, value)
+	}
+	return err
+}
+
+func IsJSON(b []byte) bool {
+	var js json.RawMessage
+	return json.Unmarshal(b, &js) == nil
+}
 
 func main() {
 	var (
@@ -15,19 +38,19 @@ func main() {
 		age                 int
 		name                string
 		female              bool
-		extHobbies          string
+		extHobbies          hobbies
 		extLinkmanParentDad string
 		extLinkmanParentMom string
 		duration            time.Duration
 	)
 
-	flag.StringVar(&config, "config", "", "help message")
+	flag.StringVar(&config, "config", "config.toml", "help message")
 	flag.StringVar(&name, "name", "", "help message")
 	flag.IntVar(&age, "age", 0, "help message")
 	flag.Float64Var(&length, "length", 0, "help message")
 	flag.BoolVar(&female, "female", false, "help message")
 	flag.DurationVar(&duration, "duration", time.Second*5, "")
-	flag.StringVar(&extHobbies, "ext.hobbies", "", "")
+	flag.Var(&extHobbies, "ext.hobbies", "")
 	flag.StringVar(&extLinkmanParentDad, "ext.linkman.parent.dad", "", "")
 	flag.StringVar(&extLinkmanParentMom, "ext.linkman.parent.mom", "", "")
 
